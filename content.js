d89cbc533f594f09b8e5786e305dd8e964301e1e
volodya-buryy <vova.buryy@yandex.com>
2017-09-08 16:11:46 +0300

    firct

---
+++ content.js
@@ -0,0 +1,10 @@
+/* Listen for messages */
+console.log(chrome)
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    /* If the received message has the expected format... */
+    if (msg.text && (msg.text == "report_back")) {
+        /* Call the specified callback, passing
+           the web-pages DOM content as argument */
+    sendResponse(document.getElementById("getText").innerHTML);
+    }
+});
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('getText').addEventListener('click', function(event) {
+	console.log(event);
+});
