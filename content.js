8c67b9a8893c206e620d85ccd134d0ef85d35c28
volodya-buryy <vova.buryy@yandex.com>
2017-09-08 22:30:56 +0300

    minor

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 /* Listen for messages */
-console.log(chrome)
+console.log(chrome.tabs)
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    console.log(tabs);
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getDOM" }, function(response) {
+        console.log(response);
+    });
+});
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     /* If the received message has the expected format... */
     if (msg.text && (msg.text == "report_back")) {
