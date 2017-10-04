fa6e5c6a50c6cee25bde9dd118a7c66e66024fec
volodya-buryy <vova.buryy@yandex.com>
2017-10-04 20:08:14 +0300

    get dom document and access to different domens

--- background.js
+++ background.js
@@ -1,18 +1,7 @@
-/* Regex-pattern to check URLs against.
-   It matches URLs like: http[s]://[...]stackoverflow.com[...] */
-var urlRegex = /^file:\/\/\/:?/;
-
-/* A function creator for callbacks */
-function doStuffWithDOM(element) {
-    alert("I received the following DOM content:\n" + element);
-}
-
-/* When the browser-action button is clicked... */
-chrome.browserAction.onClicked.addListener(function(tab) {
-    /*...check the URL of the active tab against our pattern and... */
-    if (urlRegex.test(tab.url)) {
-        /* ...if it matches, send a message specifying a callback too */
-        chrome.tabs.sendMessage(tab.id, { text: "report_back" },
-                                doStuffWithDOM);
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+    // First, validate the message's structure
+    if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
+      // Enable the page-action for the requesting tab
+      chrome.pageAction.show(sender.tab.id);
     }
-});
+  });
