c307babd37b8ce3a26338173bc7e390e3db61f0f
volodya-buryy <vova.buryy@yandex.com>
2017-10-18 17:18:16 +0300

    new pdf create pacage

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     path = require("path");
-    pdf = require('express-pdf');
+let pdf = require('html-pdf');
+var options = { format: 'Letter' };
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(express.static(path.join(__dirname, 'www')));
 
 //tell express what to do when the /about route is requested
 app.post('/create-pdf-file', function(req, res){
-    console.log(req.body)
-    res.send("!!!!!!");
-    res.pdfFromHTML({
-        filename: 'generated.pdf',
-        htmlContent: req.body.data,
+    console.log(req.body.data.data)
+    var str = req.body.data.data;
+    pdf.create(str, options).toFile(`./businesscard${new Date()}.pdf`, function(err, res) {
+    if (err) return console.log(err);
+        console.log(res); // { filename: '/app/businesscard.pdf' }
     });
+    res.send('Ok')
 });
 
 //wait for a connection
